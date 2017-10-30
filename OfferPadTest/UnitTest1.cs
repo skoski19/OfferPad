5938899d3ccc6fe52ea09eca3e820d11c834e411
Stjepan Rajko <stjepan.rajko@offerpad.com>
2017-10-30 09:05:35 -0700

    Add solution and project

---
+++ OfferPadTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OfferPadTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
