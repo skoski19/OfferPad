e98854fba23b5b944ba60b4c0253e5e9aae389da
Stjepan Rajko <stjepan.rajko@offerpad.com>
2017-11-09 12:27:28 -0700

    Add config

--- OfferPadTest/UnitTest1.cs
+++ OfferPadTest/UnitTest1.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
 
 namespace OfferPadTest
 {
     [TestClass]
-    public class UnitTest1
+    public class TestWeb
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestHomePageHasAddressInput()
         {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.offerpad.com/");
+
+                var addressInput = driver.FindElementsById("address-input").FirstOrDefault();
+
+                Assert.IsNotNull(addressInput);
+            }
+        }
+
+        [TestMethod]
+        public void TestHomePageDoesNotHaveError()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.offerpad.com/");
+
+                var error = driver.FindElementsByClassName("error").FirstOrDefault();
+
+                Assert.IsNull(error);
+            }
         }
     }
 }
